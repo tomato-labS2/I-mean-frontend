5a161b3d9b694d12958e010f5c930d6baf1304d5
minyeong-lee <lmy1366@gmail.com>
2025-05-28 14:26:17 +0900

    [feat] 배포 설정 완료

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // 정적 export를 위한 옵션
+  trailingSlash: true, //모든 경로에 슬래시 붙여서 S3에서 폴더처럼 인식
+  output: "export",
+  images: {
+    loader: "akamai", // 정적 사이트에 적합한 이미지 처리 방식
+    path: "/",        // 이미지 경로 기준
+  },
+
+  // (선택) basePath도 설정 가능
+  // basePath: '/your-subpath',
 };
 
 export default nextConfig;
