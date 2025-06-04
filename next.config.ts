953442b1c479bc69eb37f3610f3f4212e76699ce
minyeong-lee <lmy1366@gmail.com>
2025-06-05 08:11:13 +0900

    api 경로 수정

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,20 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
 
-  // 정적 export를 위한 옵션 주석 처리 또는 삭제
-  // trailingSlash: true,
-  // output: "export", 
   images: {
-    // loader: "akamai", // 정적 사이트에 적합한 이미지 처리 방식 - 필요시 주석 해제
-    // path: "/",        // 이미지 경로 기준 - 필요시 주석 해제
-    unoptimized: true, // 로컬 개발 시에는 unoptimized: true가 유용할 수 있습니다.
+    unoptimized: true,
   },
   eslint:{
     ignoreDuringBuilds: true,
   },
 
-
+  // ✅ 여기에 환경 변수 주입 추가
+  env: {
+    NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY: process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_PYTHON_API_URL: process.env.NEXT_PUBLIC_PYTHON_API_URL,
+    NEXT_PUBLIC_WS_URL: process.env.NEXT_PUBLIC_WS_URL,
+  },
 };
 
 export default nextConfig;
